00c4e1b3dbb5c4e3e632c5105eedd57e74d50e2c
brayan-coy <brayan324554@gmail.com>
2023-08-19 13:17:09 -0500

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-document
-  .getElementById("ingresarButton")
-  .addEventListener("click", function () {
-    var usuarioIngresado = prompt("Ingresa el nombre de usuario:");
-    var numeroIngresado = prompt("Ingresa el número 6789:");
+var timeoutID;
 
-    if (usuarioIngresado === "brayan" && numeroIngresado === "666") {
-      // Redirigir a otra página si el nombre de usuario y número son correctos
-      window.location.href = "otrapagina.html";
-    } else {
-      alert("Datos incorrectos. Inténtalo nuevamente.");
-    }
-  });
+function recargarPagina() {
+  window.location.reload(); // Recargar la página actual
+}
+
+function iniciarTimeout() {
+  clearTimeout(timeoutID); // Reiniciar el timeout
+  timeoutID = setTimeout(recargarPagina, 30000); // Configurar timeout para recargar después de 30 segundos
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("ingresarButton")
+    .addEventListener("click", function () {
+      iniciarTimeout(); // Reiniciar el timeout al hacer clic en el botón
+      pedirDatos();
+    });
+});
+
+function pedirDatos() {
+  var usuarioIngresado = document.getElementById("nombreUsuario").value;
+  var contrasenaIngresada = document.getElementById("contrasena").value;
+
+  if (usuarioIngresado === "brayan" && contrasenaIngresada === "666") {
+    // Redirigir a otra página si el nombre de usuario y contraseña son correctos
+    window.location.href = "otrapagina.html";
+  } else {
+    alert("Datos incorrectos. Inténtalo nuevamente.");
+    iniciarTimeout(); // Reiniciar el timeout después de un intento fallido
+  }
+}
+
+// Iniciar el timeout cuando se carga la página
+iniciarTimeout();
