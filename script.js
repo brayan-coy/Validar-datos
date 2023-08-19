9f5d5b860823fa9bb63c43fd5f1fbfc30b74b2eb
brayan-coy <brayan324554@gmail.com>
2023-08-19 17:23:48 -0500

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 var timeoutID;
-var tiempoInactividad = 30000; // 30 segundos de inactividad
 
-function recargarPagina() {
-  window.location.href = "index.html"; // Redirigir a index.html
+function redirigirAIndex() {
+  window.location.href = "index.html"; // Redirigir al index.html
 }
 
 function iniciarTimeout() {
-  clearTimeout(timeoutID);
-  timeoutID = setTimeout(recargarPagina, tiempoInactividad);
+  clearTimeout(timeoutID); // Reiniciar el timeout
+  timeoutID = setTimeout(redirigirAIndex, 30000); // Configurar timeout para redirigir después de 30 segundos
 }
-
-// Reiniciar el timeout al detectar movimiento del mouse
-document.addEventListener("mousemove", function () {
-  iniciarTimeout();
-});
 
 document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("ingresarButton")
-    .addEventListener("click", function (event) {
-      event.preventDefault();
-      iniciarTimeout();
+    .addEventListener("click", function () {
+      iniciarTimeout(); // Reiniciar el timeout al hacer clic en el botón
       pedirDatos();
     });
 });
@@ -29,13 +22,16 @@ function pedirDatos() {
   var usuarioSeleccionado = document.getElementById("usuarioSelector").value;
   var contrasenaIngresada = document.getElementById("contrasena").value;
 
+  // Verifica las credenciales según el usuario seleccionado
   if (usuarioSeleccionado === "brayan" && contrasenaIngresada === "666") {
     window.location.href = "otrapagina.html";
   } else if (usuarioSeleccionado === "pepe" && contrasenaIngresada === "777") {
     window.location.href = "otrapagina.html";
+  } else if (usuarioSeleccionado === "jorge" && contrasenaIngresada === "888") {
+    window.location.href = "otrapagina.html";
   } else {
     alert("Datos incorrectos. Inténtalo nuevamente.");
-    iniciarTimeout();
+    iniciarTimeout(); // Reiniciar el timeout después de un intento fallido
   }
 }
 
